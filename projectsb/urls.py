5bb9959d7be64420878e2b2e2f53324d12a28e32
anilwagle808 <anil.wagle808@gmail.com>
2023-11-05 13:14:48 +0545

    projectsb djanog project initiated

--- projectsb/urls.py
+++ projectsb/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ProjectSB project.
+URL configuration for projectsb project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
