468fb65371d67b445255eceed19c3e5ecd3f31ce
anilwagle808 <anil.wagle808@gmail.com>
2023-12-07 13:49:06 +0545

    Leaderboard Point Table Made

--- sportsApp/views.py
+++ sportsApp/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,'main.html')
+
+
+def leaderboard(request):
+    return render(request,'./leaderboard/leaderboard.html')
