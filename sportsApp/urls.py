545cfc89e900017b322e89e146af079441183b2e
anilwagle808 <anil.wagle808@gmail.com>
2023-12-07 15:19:27 +0545

    Leaderboard Made Responsive

--- sportsApp/urls.py
+++ sportsApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.index),
-    path('leaderboard/',views.leaderboard,name='leaderboardP'),
+    path('',views.index,name='home'),
+    path('leaderboard/',views.leaderboard,name='leaderboard'),
 ]
